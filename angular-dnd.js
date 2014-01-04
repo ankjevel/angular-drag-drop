ff03b21042e44d4102d4a7693d758775b224d03a
Dennis Pettersson <mail@dennispettersson.se>
2014-01-04 16:12:42 +0100

    offset fix, and also a dragimage

--- angular-dnd.js
+++ angular-dnd.js
@@ -1,165 +1,246 @@
 angular.module('dragAndDrop', [])
-  .directive( 'drag', function ( dndApi ) {
+.directive('drag', function (dndApi, $window) {
 
-    var drags = [];
-    var parent = function(drag, max) {
-      if(max > 5) {  return false; } /* Just incase */
-      var p = drag.parent();
-      if(p.hasClass('drop')) {
-        return p[0];
+  var drags = [];
+  var parent = function (drag, max) {
+    if (max > 5) {
+      return false;  /* Just incase */
+    }
+    var p = drag.parent();
+    if (p.hasClass('drop')) {
+      return p[0];
+    } else {
+      max++;
+      return parent(p, max);
+    }
+  };
+
+  return {
+    restrict: 'A',
+    link: function ($scope, $elem, $attr) {
+      var ngModel;
+      if (angular.isDefined($scope.node)) {
+        ngModel = $scope.node;
+      } else if (angular.isDefined($scope.data)) {
+        ngModel = $scope.data;
       } else {
-        max++;
-        return parent(p, max);
+        ngModel = $scope.$eval($attr.ngModel);
+        if (angular.isUndefined(ngModel)) {
+          return;
+        }
       }
-    };
 
-    return {
-      restrict: 'A',
-      link: function ( $scope, $elem, $attr ) {
-        var ngModel = $scope.$eval($attr.ngModel);      
-        if(angular.isUndefined(ngModel)) { return; } 
+      var clone = {
+        colors: [],
+        elem: null
+      };
 
-        var elem  = $elem[0];
-        var start = $scope.$eval($attr.start),
-            end   = $scope.$eval($attr.end);
+      if (angular.isDefined($attr.clone)) {
+        if ($attr.clone === 'mid') {
+          clone.colors = ['#2e4b65','#123453'];
+        } else {
+          clone.colors = ['#222','#101010'];
+        }
+      }
 
-        elem.addEventListener( 'dragstart', function ( e ) {
-          if(drags.length === 0) { drags = document.querySelectorAll( '.drop' ); }
+      var elem = $elem[0];
 
-          angular.forEach(dndApi.areas(), function ( value, key ) {
-            if(value[0] !== parent($elem, 0)) { value.addClass('draging'); }
+      var end = $scope.$eval($attr.end);
+
+      function renderClone() {
+        var canvas = $window.document.createElementNS('http://www.w3.org/1999/xhtml', 'canvas');
+        canvas.width = '330';
+        canvas.height = '120';
+
+        var background = canvas.getContext('2d').createLinearGradient(0, 0, 0, 310);
+        background.addColorStop(0, clone.colors[0]);
+        background.addColorStop(1, clone.colors[1]);
+
+        canvas.getContext('2d').beginPath();
+        canvas.getContext('2d').rect(0, 0, 330, 120);
+
+        canvas.getContext('2d').beginPath();
+        canvas.getContext('2d').moveTo(10, 10);
+        canvas.getContext('2d').lineTo(320, 10);
+        canvas.getContext('2d').lineTo(320, 110);
+        canvas.getContext('2d').lineTo(10, 110);
+        canvas.getContext('2d').lineTo(10, 10);
+        canvas.getContext('2d').strokeStyle = 'rgba(0,0,0,0.5)';
+        canvas.getContext('2d').stroke();
+
+        canvas.getContext('2d').shadowColor = 'rgba(0,0,0,0.5)';
+        canvas.getContext('2d').shadowBlur = 10;
+
+        canvas.getContext('2d').fillStyle = background;
+        canvas.getContext('2d').fillRect(10, 10, 310, 100);
+
+        clone.elem = $window.document.createElement('img');
+        clone.elem.src = canvas.toDataURL();
+      }
+
+      elem.addEventListener('dragstart', function (e) {
+        if (drags.length === 0) {
+          drags = angular.element.find('.drop');
+        }
+
+        angular.forEach(dndApi.areas(), function (value, key) {
+          if (value[0] !== parent($elem, 0)) {
+            value.addClass('draging');
+          }
+        });
+
+        $elem.addClass('on-drag');
+
+
+        (e.originalEvent || e).dataTransfer.effectAllowed = e.altKey ? 'copy' : 'move';
+        (e.originalEvent || e).dataTransfer.setData('text/html', '');
+
+        var offset;
+
+        if (angular.isDefined($attr.clone)) {
+          renderClone();
+          offset = 150;
+          (e.originalEvent || e).dataTransfer.setDragImage(clone.elem, 160, 60);
+        } else {
+          offset = e.offsetX;
+        }
+
+        dndApi.setData(ngModel, offset);
+      });
+
+      elem.addEventListener('dragend', function (e) {
+        var dropContent = $scope[$attr.dragContent];
+        var event = e.dataTransfer.dropEffect;
+
+        $elem.removeClass('on-drag');
+
+        angular.forEach(dndApi.areas(), function (area) {
+          area.removeClass('draging');
+
+        });
+        if (angular.isFunction(end) && angular.isDefined(dropContent)) {
+          if (event === 'move') {
+            $scope.$apply(function () {
+              end(ngModel, dropContent);
+            });
+          }
+        }
+
+        dndApi.clear();
+        clone.elem = null;
+      });
+
+      elem.setAttribute('draggable', true);
+    }
+  };
+}).directive('drop', function (dndApi) {
+
+  var areas = [];
+  var drags = [];
+
+  return {
+    link: function ($scope, $elem, $attr) {
+      var ngModel;
+
+      if (angular.isDefined($scope.node)) {
+        ngModel = $scope.node;
+      } else if (angular.isDefined($scope.data)) {
+        ngModel = $scope.data;
+      } else if (angular.isDefined($attr.ngModel)) {
+        ngModel = $scope.$eval($attr.ngModel);
+      }
+
+      var elem    = $elem[0];
+      var drop    = $scope.$eval($attr.drop),
+          enter   = $scope.$eval($attr.enter),
+          leave   = $scope.$eval($attr.leave);
+
+      var left    = elem.offsetLeft,
+          right   = left + elem.offsetWidth,
+          top     = elem.offsetTop,
+          bottom  = top + elem.offsetHeight;
+
+      dndApi.addArea($elem);
+
+      elem.addEventListener('drop', function (e) {
+
+        var result = dndApi.getData();
+
+        if (e.stopPropagation()) {
+          e.preventDefault();
+        }
+
+        if (angular.isFunction(drop) && angular.isDefined(result)) {
+
+          var dropContent = $scope[$attr.dropContent];
+
+          if (dropContent && typeof $scope.elementWidth !== "undefined") {
+            var x = (e.offsetX - result.offset) / $scope.elementWidth * 100;
+            result.data.position = {
+              x: Math.max(Math.min(x, 100), 0)
+            };
+          }
+
+          result.data.type = result.data.name.match(/\.mid$/i) ? 'mid' : 'wav';
+
+          $scope.$apply(function () {
+            drop(result.data, dropContent || false);
           });
+        }
 
-          $elem.addClass('on-drag');
-
-          dndApi.setData(ngModel, $elem);
-
-          (e.originalEvent || e).dataTransfer.effectAllowed = 'move';
-          (e.originalEvent || e).dataTransfer.setData( 'text', 'no-data' );
-
-          if(angular.isFunction(start)) {
-            $scope.$apply(function() {
-              start(dndApi.getData(), $elem);
-            });
-          }
-
+        angular.forEach(dndApi.areas(), function (area, key) {
+          area.addClass('draging');
         });
 
-        elem.addEventListener( 'dragend', function ( e ) {
+        dndApi.clear();
+      });
 
-          $elem.removeClass('on-drag');
+      elem.addEventListener('dragenter', function (e) {
+      });
 
-          angular.forEach(dndApi.areas(), function ( area, key ) {
-            area.removeClass('draging');
-          });
+      elem.addEventListener('dragleave', function (e) {
+      });
 
-          if(angular.isFunction(end)){
-            $scope.$apply(function() {
-              end(dndApi.getData(), $elem);
-            });
-          }
-          dndApi.clear();
-        });
+      elem.addEventListener('dragover', function (e) {
+        if (e.preventDefault) {
+          e.preventDefault();
+        }
+        return false;
+      });
+    }
+  };
 
-        elem.draggable = true;
-        $elem.addClass('drag');
+}).factory('dndApi', function () {
+
+  var dnd = {
+    dragObject : {}
+  };
+
+  var areas = [];
+
+  return {
+    addArea: function (area){
+      areas.push(area);
+    },
+    areas: function () {
+      return areas;
+    },
+    setData: function (data, offset) {
+      if (angular.isUndefined(data)) {
+        dnd.drag = {};
+        return;
       }
-    };
-  }).directive( 'drop', function ( dndApi ) {
-
-    var areas = [];
-    var drags = [];
-
-   
-
-    return {
-      link: function ( $scope, $elem, $attr ) {
-        
-        var elem    = $elem[0];
-        var drop    = $scope.$eval($attr.drop),
-            enter   = $scope.$eval($attr.enter),
-            leave   = $scope.$eval($attr.leave);
-            
-        var left    = elem.offsetLeft,
-            right   = left + elem.offsetWidth,
-            top     = elem.offsetTop,
-            bottom  = top + elem.offsetHeight;
-
-        dndApi.addArea($elem);
-
-        elem.addEventListener( 'drop', function ( e ) {
-          var result = dndApi.getData();
-          if(!$elem.hasClass('draging')){ return; }
-          if(e.stopPropagation()) { e.preventDefault(); }
-
-          if(angular.isFunction(drop)) {
-            $scope.$apply(function() {
-              drop(result.data, result.element);
-            });
-          }
-
-          angular.forEach(dndApi.areas(), function (area, key) {
-            area.addClass('draging');
-          });
-
-          dndApi.clear();
-        });
-
-        elem.addEventListener ('dragenter', function(e) {
-          if(elem === e.target && angular.isFunction(enter)) {
-            $scope.$apply(function() {
-              var result = dndApi.getData();
-              enter(result.data, result.element);
-            });
-          }
-        });
-
-        elem.addEventListener ( 'dragleave', function(e) {
-          if((e.x < left || e.x > right) || (e.y < top  || e.y > bottom)) {
-            if(angular.isFunction(leave)){
-              $scope.$apply(function() {
-                var result = dndApi.getData();
-                leave(result.data, result.element);
-              });
-            }
-          }
-        });
-
-        elem.addEventListener ( 'dragover', function ( e ) {
-          if (e.preventDefault) { e.preventDefault(); }
-          return false;
-        });
-
-        $elem.addClass('drop');
-      }
-    };
-
-  }).factory('dndApi', function() {
-
-    var dnd = {
-      dragObject : {}
-    };
-
-    var areas = [];
-
-    return {
-      addArea : function(area){
-        areas.push(area);
-      },
-      areas : function() {
-        return areas;
-      },
-      setData : function(data, element) {
-        dnd.drag = { data : data, element : element};
-      },
-      clear : function(){
-        delete dnd.drag;
-      },
-      getData : function(){
-        return dnd.drag;
-      }
-    };
-  });
-
-
-
+      var copy = angular.copy(data);
+      dnd.drag = {
+        data: copy,
+        offset: offset
+      };
+    },
+    clear : function (){
+      delete dnd.drag;
+    },
+    getData : function (){
+      return dnd.drag;
+    }
+  };
+});
